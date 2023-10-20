3274b6c82149aa41a3497c4c386c1c160b8e926d
clembabs <clembabs10@gmail.com>
2023-10-20 21:46:21 +0100

    Assets

--- lib/src/core/constants/svgs.dart
+++ lib/src/core/constants/svgs.dart
@@ -1,3 +1,16 @@
 class SvgIcons {
   SvgIcons._();
+
+  static const String svgPath = 'assets/icons/';
+
+  static const String homeInactiveIcon = '${svgPath}home_inactive.svg';
+  static const String homeActiveIcon = '${svgPath}home_active.svg';
+  static const String cartInactiveIcon = '${svgPath}cart_inactive.svg';
+  static const String cartActiveIcon = '${svgPath}cart_active.svg';
+  static const String favouriteInactiveIcon =
+      '${svgPath}favourite_inactive.svg';
+  static const String favouriteActiveIcon = '${svgPath}favourite_active.svg';
+  static const String profileIcon = '${svgPath}profile.svg';
+
+  static const String whiteBackground = '${svgPath}white_background.svg';
 }
